d2143aa4cf3ea62c643dab1f2320ebfcc306adf1
Tetsuo.Oda <tetsuo21ad@gmail.com>
2023-12-23 10:39:54 +0900

    feat: :package: melos を導入

--- android/app/src/main/kotlin/com/toda/pokemon_database/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.toda.pokemon_database
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ packages/app/android/app/src/main/kotlin/com/toda/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.toda.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
